f6d5c50f3be0d729b97737c6e5e2489da0c9800a
lichangyu <lcy19961018@163.com>
2025-08-14 12:08:26 +0800

    mod: modify openrouter adapters

--- lua/others/openrouter.lua
+++ lua/others/openrouter.lua
@@ -79,9 +79,8 @@ return {
     ---@param messages table
     ---@return table
     form_parameters = function(self, params, messages)
-      if self.temp.reasoning_effort then
+      if self.temp.extended_thinking then
         params.reasoning = {
-          -- effort = self.temp.reasoning_effort,
           max_tokens = self.temp.reasoning_max_tokens,
         }
       end
@@ -281,7 +280,7 @@ return {
       if not delta then return nil end
 
       local can_reason = false
-      if self.temp.reasoning_effort then can_reason = true end
+      if self.temp.extended_thinking then can_reason = true end
 
       if can_reason and delta.reasoning then
         output.reasoning = output.reasoning or {}
@@ -354,6 +353,7 @@ return {
     end,
   },
   schema = {
+    ---@type CodeCompanion.Schema
     model = {
       order = 1,
       mapping = "parameters",
@@ -369,10 +369,11 @@ return {
         ["z-ai/glm-4.5"] = { opts = { can_reason = true, has_vision = false } },
       },
     },
-    reasoning_effort = {
+    ---@type CodeCompanion.Schema
+    extended_thinking = {
       order = 2,
       mapping = "temp",
-      type = "string",
+      type = "boolean",
       optional = true,
       condition = function(self)
         local model = self.schema.model.default
@@ -382,14 +383,10 @@ return {
         end
         return false
       end,
-      default = nil,
-      desc = "Constrains effort on reasoning for reasoning models. Reducing reasoning effort can result in faster responses and fewer tokens used on reasoning in a response.",
-      choices = {
-        "high",
-        "medium",
-        "low",
-      },
+      default = false,
+      desc = "Enable extended thinking for more thorough reasoning. Requires thinking_budget to be set.",
     },
+    ---@type CodeCompanion.Schema
     reasoning_max_tokens = {
       order = 3,
       mapping = "temp",
@@ -403,9 +400,10 @@ return {
         end
         return false
       end,
-      default = 600,
-      desc = "Max tokens for reasoning",
+      default = 16000,
+      desc = "The maximum number of tokens to use for thinking when extended_thinking is enabled. Must be less than max_tokens.",
     },
+    ---@type CodeCompanion.Schema
     temperature = {
       order = 4,
       mapping = "parameters",
@@ -415,15 +413,17 @@ return {
       desc = "What sampling temperature to use, between 0 and 2. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic. We generally recommend altering this or top_p but not both.",
       validate = function(n) return n >= 0 and n <= 2, "Must be between 0 and 2" end,
     },
+    ---@type CodeCompanion.Schema
     top_p = {
       order = 5,
       mapping = "parameters",
       type = "number",
       optional = true,
-      default = 1,
+      default = nil,
       desc = "An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered. We generally recommend altering this or temperature but not both.",
       validate = function(n) return n >= 0 and n <= 1, "Must be between 0 and 1" end,
     },
+    ---@type CodeCompanion.Schema
     stop = {
       order = 6,
       mapping = "parameters",
@@ -436,15 +436,27 @@ return {
       desc = "Up to 4 sequences where the API will stop generating further tokens.",
       validate = function(l) return #l >= 1 and #l <= 4, "Must have between 1 and 4 elements" end,
     },
+    ---@type CodeCompanion.Schema
     max_tokens = {
       order = 7,
       mapping = "parameters",
       type = "integer",
       optional = true,
-      default = nil,
+      default = function(self)
+        if
+          self.schema.extended_thinking
+          and self.schema.extended_thinking.default
+          and self.schema.reasoning_max_tokens
+          and self.schema.reasoning_max_tokens.default > 0
+        then
+          return self.schema.reasoning_max_tokens.default + 1000
+        end
+        return nil
+      end,
       desc = "The maximum number of tokens to generate in the chat completion. The total length of input tokens and generated tokens is limited by the model's context length.",
       validate = function(n) return n > 0, "Must be greater than 0" end,
     },
+    ---@type CodeCompanion.Schema
     presence_penalty = {
       order = 8,
       mapping = "parameters",
@@ -454,6 +466,7 @@ return {
       desc = "Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.",
       validate = function(n) return n >= -2 and n <= 2, "Must be between -2 and 2" end,
     },
+    ---@type CodeCompanion.Schema
     frequency_penalty = {
       order = 9,
       mapping = "parameters",
@@ -463,6 +476,7 @@ return {
       desc = "Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.",
       validate = function(n) return n >= -2 and n <= 2, "Must be between -2 and 2" end,
     },
+    ---@type CodeCompanion.Schema
     logit_bias = {
       order = 10,
       mapping = "parameters",
@@ -478,6 +492,7 @@ return {
         validate = function(n) return n >= -100 and n <= 100, "Must be between -100 and 100" end,
       },
     },
+    ---@type CodeCompanion.Schema
     user = {
       order = 111,
       mapping = "parameters",
