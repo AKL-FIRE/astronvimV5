4609a8ae516307178801ac701137267d884adffe
lichangyu <lcy19961018@163.com>
2025-09-23 10:40:41 +0800

    mod: add ruff linter for python

--- lua/community.lua
+++ lua/community.lua
@@ -18,8 +18,9 @@ return {
   vim.fn.executable "npm" == 1 and { import = "astrocommunity.pack.json" } or {},
   vim.fn.executable "npm" == 1 and { import = "astrocommunity.pack.yaml" } or {},
   vim.fn.executable "npm" == 1 and { import = "astrocommunity.pack.prisma" } or {},
-  (vim.fn.executable "python" == 1 or vim.fn.executable "python3" == 1) and { import = "astrocommunity.pack.python" }
-    or {},
+  (vim.fn.executable "python" == 1 or vim.fn.executable "python3" == 1) and {
+    import = "astrocommunity.pack.python-ruff",
+  } or {},
   (vim.fn.executable "gcc" == 1 or vim.fn.executable "clang" == 1) and { import = "astrocommunity.pack.cpp" } or {},
   vim.fn.executable "cmake" == 1 and { import = "astrocommunity.pack.cmake" } or {},
   vim.fn.executable "rustc" == 1 and { import = "astrocommunity.pack.rust" } or {},
